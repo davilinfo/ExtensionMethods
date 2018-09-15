1b82d215adba2b2f73f80cb548deb72995875e11
davilimaalves <davi.alves@aurea.com>
2018-09-15 20:30:39 -0300

    Adicionando arquivos de projeto

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    public class UsingActivator
+    {
+        public string Name { get; set; }
+
+        public UsingActivator()
+        {
+
+        }
+    }
+
+    public static class MyStringExtension
+    {
+        const string Added = "added";
+        public static string CreateAnotherText(this string text, string url)
+        {
+            return $@"{url} {text} {Added}";
+        }
+    }
+
+    class Program
+    {        
+        const string NewName = "new name";
+        const string Confluence = "confluence";
+
+        static void Main(string[] args)
+        {
+            var activator = Activator.CreateInstance(typeof(UsingActivator));
+            (activator as UsingActivator).Name = "ok";
+
+            string name = NewName;
+            Console.WriteLine( name.CreateAnotherText($@"{Confluence} {(activator as UsingActivator).Name}"));            
+
+            Console.ReadLine();
+        }
+    }
+}
