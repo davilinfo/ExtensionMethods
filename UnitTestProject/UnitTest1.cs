b1c37763a7eb00e6b7535948200141dbb4aed269
davilimaalves <davi.alves@aurea.com>
2018-09-18 09:47:48 -0300

    Adicionando MSTest

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExtensionMethods;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private UsingActivator _usingActivator;
+        private const string UsingActivatorNameValue = "test";
+
+        [TestInitialize]
+        public void UnitTest1Setup()
+        {
+            _usingActivator = new UsingActivator();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            _usingActivator.Name = UsingActivatorNameValue;
+
+            Assert.IsTrue(_usingActivator.Name == UsingActivatorNameValue);
+
+        }
+    }
+}
